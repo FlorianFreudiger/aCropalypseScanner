1c7ee3d61cb9ab788481c4812498ede9536e6d1f
Florian Freudiger <25648113+FlorianFreudiger@users.noreply.github.com>
2023-04-01 23:37:53 +0200

    Use clap to parse multiple path arguments

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,59 @@
 mod png;
 
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use clap::Parser;
 use walkdir::WalkDir;
 
 use crate::png::check_file;
 
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+    #[arg(required = true, value_hint = clap::ValueHint::AnyPath)]
+    paths: Vec<PathBuf>,
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: {} <path>", args[0]);
-        return;
-    }
-    let path_argument = Path::new(&args[1]);
+    let args = Args::parse();
 
-    let mut paths: Vec<PathBuf> = vec!();
+    let mut png_paths: Vec<PathBuf> = vec!();
 
-    if path_argument.is_dir() {
-        println!("Scanning directory");
+    for path_argument in args.paths {
+        if path_argument.is_dir() {
+            println!("Scanning directory {} for png files", path_argument.display());
 
-        for entry_result in WalkDir::new(path_argument) {
-            match entry_result {
-                Ok(entry) if entry.file_type().is_file() => {
-                    let path = entry.path();
-                    let extension = match path.extension() {
-                        None => { continue; }
-                        Some(extension) => { extension }
-                    };
+            for entry_result in WalkDir::new(path_argument) {
+                match entry_result {
+                    Ok(entry) if entry.file_type().is_file() => {
+                        let path = entry.path();
+                        let extension = match path.extension() {
+                            None => { continue; }
+                            Some(extension) => { extension }
+                        };
 
-                    if extension.to_str() == Some("png") {
-                        paths.push(PathBuf::from(path));
+                        if extension.to_str() == Some("png") {
+                            png_paths.push(PathBuf::from(path));
+                        }
+                    }
+                    Ok(_) => {}
+                    Err(err) => {
+                        eprintln!("Skipping directory entry because of {}", err)
                     }
                 }
-                Ok(_) => {}
-                Err(err) => {
-                    eprintln!("Skipping directory entry because of {}", err)
-                }
             }
+        } else if path_argument.is_file() {
+            png_paths.push(path_argument.to_path_buf());
+        } else {
+            eprintln!("{} is not a valid path to a file or directory", path_argument.display());
+            return;
         }
-    } else if path_argument.is_file() {
-        paths.push(path_argument.to_path_buf());
     }
 
-    for path in paths {
-        let result = match check_file(&path) {
+    for png_path in png_paths {
+        let result = match check_file(&png_path) {
             Ok(result) => { result }
             Err(err) => {
-                eprintln!("{}: Error: {}", path.display(), err);
+                eprintln!("{}: Error: {}", png_path.display(), err);
                 continue;
             }
         };
@@ -56,11 +64,11 @@ fn main() {
         }
 
         match result.iend_count() {
-            0 => { println!("{}: Malformed png, no end", path.display()); }
+            0 => { println!("{}: Malformed png, no end", png_path.display()); }
             1 => {
                 //println!("{}: Ok", path.display());
             }
-            _ => { println!("{}: Bad crop! {} png ends detected", path.display(), result.iend_count()); }
+            _ => { println!("{}: Bad crop! {} png ends detected", png_path.display(), result.iend_count()); }
         }
     }
 }
